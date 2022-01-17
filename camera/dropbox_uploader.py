ed3836879c943f89efd2424f7a8a509394768acf
victor <victorvtf@gmail.com>
2022-01-17 13:07:39 -0800

    initial setup improved

---
+++ camera/dropbox_uploader.py
@@ -0,0 +1,21 @@
+import dropbox
+import json
+import requests
+import os
+
+DROPBOX_SECOND_HALF_TOKEN_URL = 'https://raw.githubusercontent.com/v-popov/window-collage/main/dropbox_half_token.json'
+
+if __name__ == '__main__':
+    resp = requests.get(DROPBOX_SECOND_HALF_TOKEN_URL)
+    data = json.loads(resp.text)
+    dbx = dropbox.Dropbox(f"{os.environ['FIRST_HALF_TOKEN']}{data['half-token']}")
+
+    project_abs_path = os.environ['CAMERA_DIR']
+    photos_filenames = os.listdir(f"{project_abs_path}/photos/")
+
+    # upload files to dropbox
+    for photo_filename in photos_filenames:
+        with open(f'{project_abs_path}/photos/{photo_filename}', "rb") as image:
+            f = image.read()
+            dbx.files_upload(f, f'/raw_photos/{photo_filename}')
+            os.remove(f'{project_abs_path}/photos/{photo_filename}')
--- setup.sh
+++ setup.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-chmod +x ./camera/take_photo.sh
+yes | sudo apt update && sudo apt upgrade
+yes | sudo rpi-update
 
-(crontab -l ; echo "0 * * * * /home/pi/projects/window_collage/camera/take_photo.sh")| crontab -
+mkdir projects
+cd projects
+
+touch ./env_vars.sh
+echo "export CAMERA_DIR=/home/pi/projects/window_collage/camera/" >> env_vars.sh
+echo "export DROPBOX_FIRST_HALF_TOKEN='MY_DROPBOX_FIRST_HALF_TOKEN'" >> env_vars.sh
+sudo mv ./env_vars.sh /etc/profile.d/
+
+yes | sudo apt install git
+echo git --version
+git clone -b camera git@github.com:v-popov/window-collage.git
+
+yes | sudo apt install python3-pip
+pip install -r ${CAMERA_DIR}requirements.txt
+
+chmod +x ${CAMERA_DIR}take_photo.sh
+
+# (crontab -l ; echo "0 * * * * $CAMERA_DIR/take_photo.sh")| crontab -
+# (crontab -l ; echo "10 * * * * /bin/bash -l -exec "/usr/bin/python3 $CAMERA_DIR/dropbox_uploader.py"")| crontab -
+
+(crontab -l ; echo "*/7 * * * * /bin/bash -l -exec '${CAMERA_DIR}take_photo.sh >> /home/pi/projects/window_collage/camera/log_photo.txt 2>&1''")| crontab -
+(crontab -l ; echo "*/10 * * * *  /bin/bash -l -exec '/usr/bin/python3 ${CAMERA_DIR}dropbox_uploader.py >> /home/pi/projects/window_collage/camera/log_dropbox.txt 2>&1'")| crontab -
+
+sudo raspi-config
+
+sudo reboot
