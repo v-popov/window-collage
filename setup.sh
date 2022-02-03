f986fc355e095c4151a7abbfb558c350eae7079a
victor <victorvtf@gmail.com>
2022-02-02 20:44:06 -0800

    dropbox reorganizer initial commit

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+yes | sudo apt update && sudo apt upgrade
+
+mkdir projects
+
+touch ./env_vars.sh
+echo "export SERVER_DIR=/home/pi/projects/window-collage/server/" >> env_vars.sh
+echo "export DROPBOX_FIRST_HALF_TOKEN='MY_DROPBOX_FIRST_HALF_TOKEN'" >> env_vars.sh
+sudo mv ./env_vars.sh /etc/profile.d/
+source /etc/profile.d/env_vars.sh
+
+yes | sudo apt install git
+echo git --version
+cd projects
+git clone -b server https://github.com/v-popov/window-collage.git
+
+yes | sudo apt install python3-pip
+pip install -r ${SERVER_DIR}requirements.txt
+
+EDITOR=nano crontab -e
+
+(crontab -l ; echo "10 * * * * /usr/bin/python3 ${SERVER_DIR}dropbox_reorganizer.py --first_half_dropbox_token=$DROPBOX_FIRST_HALF_TOKEN > ${SERVER_DIR}log_dropbox.txt 2>&1")| crontab -
+
+sudo reboot
