4e340b4a4120015bec78cbc03f7525ba4c458715
victor <victorvtf@gmail.com>
2022-02-02 20:24:57 -0800

    photo rotation added

--- camera/take_photo.sh
+++ camera/take_photo.sh
@@ -2,4 +2,4 @@
 
 DATE=$(date +%Y-%m-%d_%H:%M)
 
-raspistill -w 3280 -h 2464 -a 12 -a 1024 -a "%Y-%m-%d %Z%z %p:%X" -ae 64,0x00,0x8080ff,2,0,2300 -n -o ${1}photos/$DATE.jpg
+raspistill -w 3280 -h 2464 -a 12 -a 1024 -a "%Y-%m-%d %Z%z %p:%X" --rotation 270 -ae 64,0x00,0x8080ff,2,0,2300 -n -o ${1}photos/$DATE.jpg
